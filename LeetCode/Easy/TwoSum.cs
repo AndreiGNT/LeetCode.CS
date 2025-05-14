ed728c94a4f53d20cad5e0b7da05e08602432cf2
Andrei Girnet <g.books2788@gmail.com>
2025-05-14 12:46:53 +0300

    Making the tests for TwoSum more organized.

--- LeetCode/Easy/TwoSum.cs
+++ LeetCode/Easy/TwoSum.cs
@@ -26,36 +26,64 @@
 
         public void TwoSumTest()
         {
-            // test 1
-            //int target = 7;
-            //int[] nums = [0, 1, 2, 3, 4, 5, 6, 7];
-            ////            0  1  2  3  4  5  6  7
-
-            //// test 2
-            //int target = 13;
-            //int[] nums = [9, 3, 7, 2, 1, 10, 5, 4];
-            ////            0  1  2  3  4   5  6  7
-
-            //// test 3
-            //int target = 11;
-            //int[] nums = [1, 1, 1, 1, 1, 9, 2, 1];
-            ////            0  1  2  3  4  5  6  7
-
-            // test 4
-            int target = 1;
-            int[] nums = [0, 0, 1];
-            //            0  1  2 
-
-            int[]? display = TwoSumFunc(nums, target);
-
-            if (display != null)
+            int target1 = 7;
+            int[] nums1 = [0, 1, 2, 3, 4, 5, 6, 7];
+            //             0  1  2  3  4  5  6  7
+            int[]? result1 = TwoSumFunc(nums1, target1);
+            if (result1 != null)
             {
-                Console.WriteLine(display[0]);
-                Console.WriteLine(display[1]);
+                Console.WriteLine("The indicies: " + result1[0]);
+                Console.WriteLine(" " + result1[1]);
             }
             else
             {
-                Console.WriteLine("Null");
+                Console.WriteLine("There is no numbers that sum up to the target");
+            }
+
+            // test 2
+            int target2 = 13;
+            int[] nums2 = [9, 3, 7, 2, 1, 10, 5, 4];
+            //            0  1  2  3  4   5  6  7
+            int[]? result2 = TwoSumFunc(nums2, target2);
+            if (result1 != null)
+            {
+                Console.WriteLine("The indicies: " + result2[0]);
+                Console.WriteLine(" " + result2[1]);
+            }
+            else
+            {
+                Console.WriteLine("There is no numbers that sum up to the target");
+            }
+
+
+            // test 3
+            int target3 = 11;
+            int[] nums3 = [1, 1, 1, 1, 1, 9, 2, 1];
+            //            0  1  2  3  4  5  6  7
+            int[]? result3 = TwoSumFunc(nums3, target3);
+            if (result1 != null)
+            {
+                Console.WriteLine("The indicies: " + result3[0]);
+                Console.WriteLine(" " + result3[1]);
+            }
+            else
+            {
+                Console.WriteLine("There is no numbers that sum up to the target");
+            }
+
+            // test 4
+            int target4 = 1;
+            int[] nums4 = [0, 0, 1];
+            //            0  1  2 
+            int[]? result4 = TwoSumFunc(nums4, target4);
+            if (result1 != null)
+            {
+                Console.WriteLine("The indicies: " + result4[0]);
+                Console.WriteLine(" " + result4[1]);
+            }
+            else
+            {
+                Console.WriteLine("There is no numbers that sum up to the target");
             }
         }
     }
