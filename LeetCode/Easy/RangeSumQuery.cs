9cf776a3ddb232088b9b0df1884c9a4cd11c2cb8
AndreiGNT <aagirnet@gmail.com>
2025-06-12 13:06:32 +0300

    I have solved the alghoritmic problem Range Sum Query.

---
+++ LeetCode/Easy/RangeSumQuery.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    public class RangeSumQuery
+    {
+        private List<int> _prefixSums = [];
+        public RangeSumQuery(int[] nums)
+        {
+            int sum = 0;
+            for (int i =  0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                _prefixSums.Add(sum);
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            int sum = _prefixSums[right];
+            if (left > 0)
+                sum -= _prefixSums[left - 1];
+
+            return sum;
+        }
+
+        // Testing cases
+        //
+        //int[] nums = { -2, 0, 3, -5, 2, -1 };
+        //RangeSumQuery rangeSumQuery = new RangeSumQuery(nums);
+        //int result = rangeSumQuery.SumRange(0, 3);
+        //Console.WriteLine(result);
+
+        //int result1 = rangeSumQuery.SumRange(3, 5);
+        //Console.WriteLine(result1);
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,8 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        MaxProfit maxProfit = new MaxProfit();
-        maxProfit.MaxProfitTest();
+        int[] nums = { -2, 0, 3, -5, 2, -1 };
+        RangeSumQuery rangeSumQuery = new RangeSumQuery(nums);
+        int result = rangeSumQuery.SumRange(0, 3);
+        Console.WriteLine(result);
+
+        int result1 = rangeSumQuery.SumRange(3, 5);
+        Console.WriteLine(result1);
 
 
     }
