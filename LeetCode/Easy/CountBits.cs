918df5e51919de40fff20cc6c900319982f739c4
AndreiGNT <aagirnet@gmail.com>
2025-06-19 23:58:22 +0300

    Solving the CountBits problem.

---
+++ LeetCode/Easy/CountBits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class CountBits
+    {
+        public int[] CountBitsFunc(int n)
+        {
+            int[] onesNumber = new int[n + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                string binary = Convert.ToString(i, 2);
+                int totalOnes = 0;
+
+                foreach (char value in binary)
+                    if (value == '1')
+                        totalOnes++;
+
+                onesNumber[i] = totalOnes;
+            }
+
+            return onesNumber;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,14 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] nums = { -2, 0, 3, -5, 2, -1 };
-        RangeSumQuery rangeSumQuery = new RangeSumQuery(nums);
-        int result = rangeSumQuery.SumRange(0, 3);
-        Console.WriteLine(result);
-
-        int result1 = rangeSumQuery.SumRange(3, 5);
-        Console.WriteLine(result1);
-
+        
 
     }
 }
