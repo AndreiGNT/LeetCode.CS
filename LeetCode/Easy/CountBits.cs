3c8985e8300abbba20783e7c1a1886aa1ac05ecd
AndreiGNT <aagirnet@gmail.com>
2025-06-20 00:50:16 +0300

    I have implemented a better solution for the problem Count Bits.

--- LeetCode/Easy/CountBits.cs
+++ LeetCode/Easy/CountBits.cs
@@ -11,6 +11,42 @@ namespace LeetCode.Easy
         public int[] CountBitsFunc(int n)
         {
             int[] onesNumber = new int[n + 1];
+            int offset = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (offset * 2 == i)
+                    offset = i;
+
+                onesNumber[i] = 1 + onesNumber[i - offset];
+            }
+
+            return onesNumber;
+        }
+
+        public void CountBitsTest()
+        {
+            int n = 10;
+            int[] result = CountBitsFunc(n);
+            Console.WriteLine("Result: ");
+            foreach (int num in result)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            int n1 = 4;
+            int[] result1 = CountBitsFunc(n1);
+            Console.WriteLine("Result: ");
+            foreach (int num in result1)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+        public int[] CountBitsFunc1(int n)
+        {
+            int[] onesNumber = new int[n + 1];
 
             for (int i = 0; i <= n; i++)
             {
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        CountBits countBits = new CountBits();
+        countBits.CountBitsTest();
 
     }
 }
