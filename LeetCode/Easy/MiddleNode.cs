86a9282860913371b6c61b3cbd25b73cd7f41299
AndreiGNT <aagirnet@gmail.com>
2025-07-04 23:28:37 +0300

    I have solved MiddleNode problem.

---
+++ LeetCode/Easy/MiddleNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class MiddleNode
+    {
+        public ListNode MiddleNodeFunc(ListNode head)
+        {
+            ListNode fast = head;
+            while (fast != null && fast.next != null)
+            {
+                head = head.next;
+                fast = fast.next.next;
+            }
+
+            return head;
+        }
+    }
+}
